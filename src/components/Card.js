d4c6fd22bea8005460925a55c0f863b2d02ab6ba
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2020-12-12 14:30:55 -0800

    add select option and state #1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Data from './Data';
 import '../index.scss';
 
-export default function Card() {
-  return (
-    <>
-      {Data.map(({
+export default class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: Data,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      characters: Data
+    })
+  }
+
+  handleChange = (val) => {
+    if (val.target.value === 'All') {
+      this.setState({
+        characters: Data
+      })
+    }
+
+    if (val.target.value === 'DragonBall Z') {
+      this.setState({
+        characters: Data.filter(i => i.topic === 'Dragonball Z')
+      })
+    }
+
+    if (val.target.value === 'Dragonball') {
+      this.setState({
+        characters: Data.filter(i => i.topic === 'Dragonball')
+      })
+    }
+  }
+
+  renderCharacters() {
+    if (this.state.characters) {
+      return this.state.characters.map(({
           img,
           topic,
           name
-        }) =>
-          <div className="card">
+        }) => (
+          <div className="card" key={name}>
+            <span>Free</span>
+
             <img src={img.url} alt={img.alt} />
-            <p>{topic}</p>
-            <h3>{name}</h3>
-            <button>
-              Save
-            </button>
+
+            <div className="details">
+              <small>{topic}</small>
+              <h2>{name}</h2>
+            </div>
+
+            <a href={img.url} download={img.url}>
+              Save {name}
+            </a>
           </div>
         )
-      }
-    </>
-  )
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <select onChange={(val) => this.handleChange(val)}>
+          <option>All</option>
+          <option>DragonBall Z</option>
+          <option>Dragonball</option>
+          <option>Star Wars</option>
+        </select>
+
+        {this.renderCharacters()}
+      </div>
+
+    )
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from './Card';
 
-export default function Home() {
-  return (
-    <div className="home">
-      <section className="slogan">
-        <h3>Free Wallpaper Backgrounds!</h3>
-      </section>
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <div className="home">
 
-      <section className="main">
+        <section className="slogan">
+          <h3>Free Wallpaper Backgrounds!</h3>
+        </section>
+
         <Card />
-      </section>
-    </div>
-  )
+      </div>
+    )
+  }
 }
