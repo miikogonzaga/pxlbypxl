30310ceff679eb232edc6475eae2d4b5d72dadf5
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2020-12-30 20:53:39 -0800

    Updates

---
+++ precache-manifest.232a29fa12d41c9708897baad897821d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4e316c1d600d2ea6ccc09a23bee4d8",
+    "url": "/pxlbypxl/index.html"
+  },
+  {
+    "revision": "4519e90baefb6961cd77",
+    "url": "/pxlbypxl/static/css/main.6f46276d.chunk.css"
+  },
+  {
+    "revision": "31c7fe39418b4e975b97",
+    "url": "/pxlbypxl/static/js/2.d1d651cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pxlbypxl/static/js/2.d1d651cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4519e90baefb6961cd77",
+    "url": "/pxlbypxl/static/js/main.d6d4390e.chunk.js"
+  },
+  {
+    "revision": "631ac94c9da5b3282083",
+    "url": "/pxlbypxl/static/js/runtime-main.08dccac3.js"
+  },
+  {
+    "revision": "096172bbe9243ade0b9e21e25b4e6c61",
+    "url": "/pxlbypxl/static/media/Gohan.096172bb.png"
+  },
+  {
+    "revision": "6ba9070306d0b85d7a420c4a394ad72e",
+    "url": "/pxlbypxl/static/media/pichu-wallpaper.6ba90703.png"
+  },
+  {
+    "revision": "98845e457d196ccc9e4896256e41a649",
+    "url": "/pxlbypxl/static/media/pichu.98845e45.png"
+  },
+  {
+    "revision": "70f9f8b97f05cd86edca6bedb4500366",
+    "url": "/pxlbypxl/static/media/squirtle-wp.70f9f8b9.png"
+  },
+  {
+    "revision": "0fa7496b44955216b86e9d6290b1f941",
+    "url": "/pxlbypxl/static/media/squirtle.0fa7496b.png"
+  },
+  {
+    "revision": "c87400e50c564ca044ae58f6d8786f36",
+    "url": "/pxlbypxl/static/media/vulpix-wp.c87400e5.png"
+  },
+  {
+    "revision": "78e05d0f325229191df0d336af63c70c",
+    "url": "/pxlbypxl/static/media/vulpix.78e05d0f.png"
+  }
+]);
