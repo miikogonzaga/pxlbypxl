c4029bf192a53c1262a87a22bfb893a03390d019
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2021-01-01 17:19:10 -0800

    update favicon / add google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import '../index.scss';
 import Nav from './Nav'
 import Home from './Home'
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('UA-114584547-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <Nav />
