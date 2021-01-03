0add6aaa70597e344f13bfda95dffcbc8b8380eb
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2021-01-02 22:33:15 -0800

    filter characters

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,22 +3,13 @@ import Gohan from '../images/Gohan.png'
 import Pichu from '../images/pichu/pichu.png'
 import PichuWallpaper from '../images/pichu/pichu-wallpaper.png'
 
-import Vulpix from '../images/vulpix/vulpix.png'
-import VulpixWallpaper from '../images/vulpix/vulpix-wp.png'
-
-import Squirtle from '../images/squirtle/squirtle.png'
-import SquirtleWallpaper from '../images/squirtle/squirtle-wp.png'
+// import Vulpix from '../images/vulpix/vulpix.png'
+// import VulpixWallpaper from '../images/vulpix/vulpix-wp.png'
+//
+// import Squirtle from '../images/squirtle/squirtle.png'
+// import SquirtleWallpaper from '../images/squirtle/squirtle-wp.png'
 
 const Characters = [
-  {
-    topic: 'Pokémon',
-    name: 'Squirtle',
-    img: {
-      url: Squirtle,
-      alt: 'Squirtle',
-      wallpaper: SquirtleWallpaper
-    }
-  },
   {
     topic: 'Pokémon',
     name: 'Pichu',
@@ -28,15 +19,24 @@ const Characters = [
       wallpaper: PichuWallpaper
     }
   },
-  {
-    topic: 'Pokémon',
-    name: 'Vulpix',
-    img: {
-      url: Vulpix,
-      alt: 'Vulpix',
-      wallpaper: VulpixWallpaper
-    }
-  },
+  // {
+  //   topic: 'Pokémon',
+  //   name: 'Vulpix',
+  //   img: {
+  //     url: Vulpix,
+  //     alt: 'Vulpix',
+  //     wallpaper: VulpixWallpaper
+  //   }
+  // },
+  // {
+  //   topic: 'Pokémon',
+  //   name: 'Squirtle',
+  //   img: {
+  //     url: Squirtle,
+  //     alt: 'Squirtle',
+  //     wallpaper: SquirtleWallpaper
+  //   }
+  // },
   {
     topic: 'Dragonball Z',
     name: 'Gohan',
