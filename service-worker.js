7ded07e464ed09f5ff46b967cb305ec893252402
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2021-01-02 22:33:53 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82cf89259623dd5b0f0370e4d199b1b0.js"
+  "/precache-manifest.751aaa549ed8f3463762c900216aec28.js"
 );
 
 self.addEventListener('message', (event) => {
