9a830a7de158ec0e43f0cff753108147cc22e5dd
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2021-01-05 21:53:24 -0800

    add vulpix #4

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,8 +3,8 @@ import Gohan from '../images/Gohan.png'
 import Pichu from '../images/pichu/pichu.png'
 import PichuWallpaper from '../images/pichu/pichu-wallpaper.png'
 
-// import Vulpix from '../images/vulpix/vulpix.png'
-// import VulpixWallpaper from '../images/vulpix/vulpix-wp.png'
+import Vulpix from '../images/vulpix/vulpix.png'
+import VulpixWallpaper from '../images/vulpix/vulpix-wp.png'
 //
 // import Squirtle from '../images/squirtle/squirtle.png'
 // import SquirtleWallpaper from '../images/squirtle/squirtle-wp.png'
@@ -19,15 +19,15 @@ const Characters = [
       wallpaper: PichuWallpaper
     }
   },
-  // {
-  //   topic: 'Pokémon',
-  //   name: 'Vulpix',
-  //   img: {
-  //     url: Vulpix,
-  //     alt: 'Vulpix',
-  //     wallpaper: VulpixWallpaper
-  //   }
-  // },
+  {
+    topic: 'Pokémon',
+    name: 'Vulpix',
+    img: {
+      url: Vulpix,
+      alt: 'Vulpix',
+      wallpaper: VulpixWallpaper
+    }
+  },
   // {
   //   topic: 'Pokémon',
   //   name: 'Squirtle',
