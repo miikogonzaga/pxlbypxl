ade8a3de6c986c3a9775545243d1ba44e1653728
gabrielgonzaga <gabrielmgonzaga@gmail.com>
2021-01-08 18:48:12 -0800

    add character #4

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,9 +5,9 @@ import PichuWallpaper from '../images/pichu/pichu-wallpaper.png'
 
 import Vulpix from '../images/vulpix/vulpix.png'
 import VulpixWallpaper from '../images/vulpix/vulpix-wp.png'
-//
-// import Squirtle from '../images/squirtle/squirtle.png'
-// import SquirtleWallpaper from '../images/squirtle/squirtle-wp.png'
+
+import Squirtle from '../images/squirtle/squirtle.png'
+import SquirtleWallpaper from '../images/squirtle/squirtle-wp.png'
 
 const Characters = [
   {
@@ -28,15 +28,15 @@ const Characters = [
       wallpaper: VulpixWallpaper
     }
   },
-  // {
-  //   topic: 'Pokémon',
-  //   name: 'Squirtle',
-  //   img: {
-  //     url: Squirtle,
-  //     alt: 'Squirtle',
-  //     wallpaper: SquirtleWallpaper
-  //   }
-  // },
+  {
+    topic: 'Pokémon',
+    name: 'Squirtle',
+    img: {
+      url: Squirtle,
+      alt: 'Squirtle',
+      wallpaper: SquirtleWallpaper
+    }
+  },
   {
     topic: 'Dragonball Z',
     name: 'Gohan',
